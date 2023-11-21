12513fd67df595762a70455a9d09472085c7f81a
ycum <dudcjf3232@naver.com>
2023-11-21 17:20:38 +0900

    Change Login Design And Make Main Page(Fragment..)_V6 add to activity calendar V2

--- app/src/main/java/com/example/myapplication/view/fragment/CalendarFragment.java
+++ app/src/main/java/com/example/myapplication/view/fragment/CalendarFragment.java
@@ -8,12 +8,43 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.R;
+import com.example.myapplication.view.object.SaturdayDecorator;
+import com.example.myapplication.view.object.SundayDecorator;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class CalendarFragment extends Fragment {
+
+    MaterialCalendarView calendarView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_calendar2, container, false);
+        calendarView = v.findViewById(R.id.calendarView);
+
+        init();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_calendar, container, false);
+        return v;
+    }
+
+    private void init(){
+        calendarView.state().edit()
+                .setFirstDayOfWeek(Calendar.SUNDAY)
+                .setMinimumDate(CalendarDay.from(1900,1,1))
+                .setMaximumDate(CalendarDay.from(5000, 12, 31))
+                .setCalendarDisplayMode(CalendarMode.MONTHS)
+                .commit();
+
+        calendarView.addDecorators(
+                new SundayDecorator(),
+                new SaturdayDecorator());
+
     }
 }
---
+++ app/src/main/java/com/example/myapplication/view/object/SundayDecorator.java
@@ -0,0 +1,31 @@
+package com.example.myapplication.view.object;
+
+import android.graphics.Color;
+import android.text.style.ForegroundColorSpan;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+
+import java.util.Calendar;
+
+public class SundayDecorator implements DayViewDecorator {
+
+    private final Calendar calendar = Calendar.getInstance();
+
+    public SundayDecorator() {
+    }
+
+    @Override
+    public boolean shouldDecorate(CalendarDay day) {
+        day.copyTo(calendar);
+        int weekDay = calendar.get(Calendar.DAY_OF_WEEK);
+        return weekDay == Calendar.SUNDAY;
+    }
+
+    @Override
+    public void decorate(DayViewFacade view) {
+        view.addSpan(new ForegroundColorSpan(Color.rgb(226, 117, 117)));
+    }
+}
+// 출처: https://goatlab.tistory.com/entry/Android-Studio-커스텀-CalendarView-1 [GOATLAB:티스토리]
